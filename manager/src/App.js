ec4ce6c71c0394d80b92f412b9b72ce204b2e306
Kamran Tasharofi <kamrant@buildingiq.com>
2016-11-24 14:28:07 +1100

    New projects such as navigation and authorisation added

---
+++ manager/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import Router from './Router';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: "AIzaSyCSBKsdkS32snuWCszoj4_hvW-Uh8zM1rM",
+    authDomain: "manager-7ece9.firebaseapp.com",
+    databaseURL: "https://manager-7ece9.firebaseio.com",
+    storageBucket: "manager-7ece9.appspot.com",
+    messagingSenderId: "672855008084"
+  };
+    firebase.initializeApp(config);
+  }
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
+
+export default App;
